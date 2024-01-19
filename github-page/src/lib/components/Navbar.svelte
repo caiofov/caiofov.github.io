2c53b552a684b9873f92046dd5b6e8acde1d043c
Caio Oliveira <cfoviana@alu.ufc.br>
2024-01-19 19:48:31 -0300

    feat: initial components

---
+++ github-page/src/lib/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Navbar, NavBrand, NavUl, NavLi, NavHamburger } from 'flowbite-svelte';
+	import NavbarItems from '../assets/NavbarItems.json';
+	import { _ } from 'svelte-i18n';
+	interface Item {
+		itemId: string;
+		icon: string;
+	}
+
+	function getItemTranslation(item: Item) {
+		const id = item.itemId.replace('#', '');
+		return $_(`sections.${id}.title`);
+	}
+</script>
+
+<Navbar id="navbar">
+	<NavBrand>
+		<span>Caio de Freitas Oliveira</span>
+	</NavBrand>
+	<NavHamburger />
+
+	<NavUl>
+		{#each NavbarItems as item}
+			<NavLi href={item.itemId}>{getItemTranslation(item)}</NavLi>
+		{/each}
+	</NavUl>
+</Navbar>
---
+++ github-page/src/lib/sections/AboutMe.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+</script>
+
+<h2 id="#about-me">{$_('sections.about-me.title')}</h2>
