929041f1fef1a9207ade05d65b88d4a6ed652646
Caio Oliveira <cfoviana@alu.ufc.br>
2024-01-19 21:35:42 -0300

    feat: flag icons

---
+++ github-page/src/lib/assets/img/flags/FlagIcon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let title = '';
+	export let viewBox = '';
+</script>
+
+<svg {viewBox}>
+	<title>{title}</title>
+	<slot />
+</svg>
+
+<style>
+	svg {
+		height: 1rem;
+		width: auto;
+		min-width: 1rem;
+	}
+</style>
