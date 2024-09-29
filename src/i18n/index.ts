dacd3856ef74984af37806c9e4c6853e352002c1
Caio Oliveira <cfoviana@alu.ufc.br>
2024-09-29 12:29:06 -0300

    translation

---
+++ src/i18n/index.ts
@@ -0,0 +1,29 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import translationsInEng from "../locales/en/translation.json";
+import translationsInPortuguese from "../locales/pt/translation.json";
+
+// the translations
+const resources = {
+  en: {
+    translation: translationsInEng,
+  },
+  pt: {
+    translation: translationsInPortuguese,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "pt",
+  debug: true,
+  fallbackLng: "pt",
+  interpolation: {
+    escapeValue: false,
+  },
+  ns: "translation",
+  defaultNS: "translation",
+});
+
+export default i18n;
