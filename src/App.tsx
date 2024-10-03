25ceb47f6736b3995d68fdf50ae6d9683676e9af
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-03 11:40:45 -0300

    refactor: use appshell structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import "./App.css";
-import { NavBar } from "./components/NavBar";
+import { Navbar } from "./components/Navbar";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Experience } from "./components/sections/Experience";
 import { Projects } from "./components/sections/Projects";
-import { MantineProvider, Container, DEFAULT_THEME } from "@mantine/core";
+import { MantineProvider, DEFAULT_THEME, AppShell } from "@mantine/core";
 
 function App() {
   return (
-    <MantineProvider theme={DEFAULT_THEME}>
-      <NavBar />
-      <Container fluid>
-        <Home />
-        <About />
-        <Experience />
-        <Projects />
-      </Container>
+    <MantineProvider theme={DEFAULT_THEME} defaultColorScheme="dark">
+      <AppShell navbar={{ width: "30%", breakpoint: "sm" }}>
+        <Navbar />
+        <AppShell.Footer>Footer</AppShell.Footer>
+
+        <AppShell.Main>
+          <AppShell.Section>
+            <Home />
+          </AppShell.Section>
+          <AppShell.Section>
+            <About />
+          </AppShell.Section>
+          <AppShell.Section>
+            <Experience />
+          </AppShell.Section>
+          <AppShell.Section>
+            <Projects />
+          </AppShell.Section>
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
   );
 }
