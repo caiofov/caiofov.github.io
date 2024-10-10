277120bb56ca8839bb8c1582bc473c19003b536e
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-10 13:25:32 -0300

    feat: opacity reveal on visible

--- src/components/animations/OpacityReveal.tsx
+++ src/components/animations/OpacityReveal.tsx
@@ -1,6 +1,45 @@
-import { motion } from "framer-motion";
-import React from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+import React, { useEffect, useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
+
+export const OpacityRevealOnVisible: React.FC<{
+  children: React.ReactNode;
+  delay?: number;
+  stiffness?: number;
+}> = ({ children, delay = 0, stiffness = 20 }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    mainControls.start(isInView ? "visible" : "hidden");
+  }, [isInView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      variants={{
+        hidden: {
+          opacity: 0,
+        },
+        visible: {
+          opacity: 1,
+        },
+      }}
+      initial="hidden"
+      animate={mainControls}
+      transition={{
+        times: [0, 1],
+        type: "spring",
+        stiffness: stiffness,
+        ease: "easeInOut",
+        delay: delay,
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+};
 
 export const OpacityReveal: React.FC<{
   children: React.ReactNode;
