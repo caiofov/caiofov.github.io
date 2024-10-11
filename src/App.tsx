7f2778643cb4f01d85fbfda4a1cb0218249b98a5
Caio Oliveira <cfoviana@alu.ufc.br>
2024-10-11 20:40:53 -0300

    feat: custom theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { MantineProvider } from "@mantine/core";
 import { useState } from "react";
 import { AppShell } from "./AppShell";
+import theme from "./theme";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
 
   return (
-    <MantineProvider forceColorScheme={darkMode ? "dark" : "light"}>
+    <MantineProvider
+      theme={theme}
+      forceColorScheme={darkMode ? "dark" : "light"}
+    >
       <AppShell darkMode={darkMode} setDarkMode={setDarkMode} />
     </MantineProvider>
   );
---
+++ src/theme.ts
@@ -0,0 +1,32 @@
+import { createTheme } from "@mantine/core";
+
+const theme = createTheme({
+  colors: {
+    customDark: [
+      "#008c9a",
+      "#007b88",
+      "#006c78",
+      "#005b67",
+      "#004c58",
+      "#003c48",
+      "#002d39",
+      "#001d2b",
+      "#041626",
+      "#000b12",
+    ],
+    customLight: [
+      "#f7f8f9",
+      "#e6eaed",
+      "#d9dce0",
+      "#cdd0d3",
+      "#c2c6c9",
+      "#b7bcc0",
+      "#acb1b5",
+      "#a1a6ab",
+      "#979da1",
+      "#8c9196",
+    ],
+  },
+});
+
+export default theme;
